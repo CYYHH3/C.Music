384a2410bac7239722e5f80392019958a45c574b
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-09-13 11:42:27 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -9,35 +9,35 @@ const ap = new APlayer({
             name: '起风了',
             artist: '周深',
             url: url+'/music/起风了/起风了.mp3',
-            cover: url+'/music/起风了/起风了.png',
+            cover: url+'/music/起风了/起风了-compressed.png',
             lrc: url+'/music/起风了/起风了.lrc'
         },
         {
             name: 'Let Yourself Go',
             artist: 'Ooyy / Snake City',
             url: url+'/music/Let Yourself Go/Let Yourself Go.mp3',
-            cover: url+'/music/Let Yourself Go/Let Yourself Go.png',
+            cover: url+'/music/Let Yourself Go/Let Yourself Go-compressed.png',
             lrc: url+'/music/Let Yourself Go/Let Yourself Go.lrc'
         },
         {
             name: '恋のうた',
             artist: 'Yunomi / 鬼頭明里',
             url: url+'/music/恋のうた/恋のうた.mp3',
-            cover: url+'/music/恋のうた/恋のうた.png',
+            cover: url+'/music/恋のうた/恋のうた-compressed.png',
             lrc: url+'/music/恋のうた/恋のうた.lrc'
         },
         {
             name: '恋のうた (Short Version)',
             artist: 'Yunomi',
             url: url+'/music/恋のうた/恋のうた (Short Version).mp3',
-            cover: url+'/music/恋のうた/恋のうた.png',
+            cover: url+'/music/恋のうた/恋のうた-compressed.png',
             lrc: url+'/music/恋のうた/恋のうた (Short Version).lrc'
         },
         {
             name: 'Lose Control',
             artist: 'Hedley',
             url: url+'/music/Lose Control/Lose Control.mp3',
-            cover: url+'/music/Lose Control/Hello.png',
+            cover: url+'/music/Lose Control/Hello-compressed.png',
             lrc: url+'/music/Lose Control/Lose Control.lrc'
         },
         {
@@ -51,21 +51,21 @@ const ap = new APlayer({
             name: 'Love and War',
             artist: 'Fleurie',
             url: url+'/music/Love and War/Love and War.mp3',
-            cover: url+'/music/Love and War/Love and War.png',
+            cover: url+'/music/Love and War/Love and War-compressed.png',
             lrc: url+'/music/Love and War/Love and War.lrc'
         },
         {
             name: "Ready Let's Go",
             artist: 'DARKMINDS',
             url: url+'/music/Ready Lets Go/Ready Lets Go.mp3',
-            cover: url+'/music/Ready Lets Go/Ready Lets Go.png',
+            cover: url+'/music/Ready Lets Go/Ready Lets Go-compressed.png',
             lrc: url+'/music/Ready Lets Go/Ready Lets Go.lrc'
         },
         {
             name: "Dimples",
             artist: 'oomiee',
             url: url+'/music/Dimples/Dimples.mp3',
-            cover: url+'/music/Dimples/Dimples.png',
+            cover: url+'/music/Dimples/Dimples-compressed.jpg',
             lrc: url+'/music/Dimples/Dimples.lrc',
         }
     ]
